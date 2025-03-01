56abb60980b25201a7cfb83a879087af582e3309
Nikhilpn <nikhilpnnick1234@gmail.com>
2025-03-01 03:12:14 -1200

    object_oriented programs

---
+++ oopss/Nikhil/single.py
@@ -0,0 +1,23 @@
+class Person:
+    def setvalue1(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+
+class Employee(Person):
+
+    def setvalue2(self,phone,job):
+        self.phone=phone
+        self.job=job
+    
+    def printvalue(self):
+        print(self.name)
+        print(self.job)
+        print(self.phone)
+        print(self.place)
+employee1=Employee()
+employee1.setvalue1("vinay",32,"thrissur")
+employee1.setvalue2(1234567890,"software engineer")
+
+employee1.printvalue()
+
