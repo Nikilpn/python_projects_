ecc566f79b2af00acc4fc81609dbff7ea13bf767
Nikhilpn <nikhilpnnick1234@gmail.com>
2025-03-03 10:44:26 -1200

    hangman game

--- Hangman/hangman_game.py
+++ Hangman/hangman_game.py
@@ -1,5 +1,39 @@
+import hangman_stages
+import word_file
 import random
 word_list=["apple","beautiful","potato"]
+lives=6
 
-chosen_word=random.choice(word_list)
-# print(chosen_word)
+
+chosen_word=random.choice(word_file.words) #generate a random word(apple)
+print(chosen_word)
+
+display=[]
+for i in range(len(chosen_word)):  #generating (_____) as how much letter present in the letter
+    display+="_"
+print(display)
+
+
+game_over=False
+
+while not game_over:#while loops iterates for whether a condition becomes true
+
+    guessed_letter = input("guessed letter :").lower()
+    for position in range(len(chosen_word)):
+        letter=chosen_word[position]
+
+        if letter==guessed_letter:
+            display[position]=guessed_letter
+    print(display)
+
+
+    if guessed_letter not in chosen_word:
+        lives-=1
+        if lives==0:
+            game_over=True
+            print("you lose")
+    if '_' not in display:
+        game_over=True
+        print("You win")
+    print(hangman_stages.stages[lives])
+
---
+++ Hangman/word_file.py
@@ -0,0 +1,7 @@
+words = [
+    "python", "developer", "keyboard", "monitor", "elephant",
+    "giraffe", "astronaut", "hangman", "universe", "chocolate",
+    "bicycle", "adventure", "mountain", "waterfall", "lightning",
+    "sandwich", "backpack", "airplane", "fireworks", "whisper",
+    "mystery", "telescope", "penguin", "ocean", "sunflower"
+]
