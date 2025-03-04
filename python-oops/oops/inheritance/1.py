712975c045fdca457ceeffa360dab7fcc9f504e8
Nikhilpn <nikhilpnnick1234@gmail.com>
2025-03-04 10:08:40 -1200

    Removed python-oops from repository

--- python-oops/oops/inheritance/1.py
+++
@@ -1,9 +0,0 @@
-class Circle:
-    pi=3.14
-    def __init__(self,radius):
-        self.radius=radius
-    def get_circum(self):
-        return  2* self.pi * self.radius
-
-circle_1=Circle(4)
-print(circle_1.get_circum())
