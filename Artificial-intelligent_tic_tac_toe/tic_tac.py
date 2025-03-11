677fc7275a08743763ac17fade038064098cd2ae
Nikhilpn <nikhilpnnick1234@gmail.com>
2025-03-11 07:43:55 -1200

    ai_tic_tac_toe

--- Artificial-intelligent_tic_tac_toe/tic_tac.py
+++ Artificial-intelligent_tic_tac_toe/tic_tac.py
@@ -9,7 +9,7 @@ def display_board(board):
     print(f'{board[1]}  |  {board[2]} |  {board[3]}')
     print(f'{board[4]}  |  {board[5]} |  {board[6]}')
     print(f'{board[7]}  |  {board[8]} |  {board[9]}')
-    print('_'*30)
+    print('_'*10)
     
     
     
@@ -30,11 +30,31 @@ def check_win():
         return True
     else:
         return False
+
+def is_win(letter):
+    if board[1]==board[2]==board[3] and board[1]==letter:
+        return True
+    elif board[4]==board[5]==board[6] and board[4]==letter:
+        return True
+    elif board[7]==board[8]==board[9] and board[7]==letter:
+        return True
+    elif board[1]==board[4]==board[7] and board[1]==letter:
+        return True
+    elif board[2]==board[5]==board[8] and board[2]==letter:
+        return True
+    elif board[1]==board[5]==board[9] and board[1]==letter:
+        return True
+    elif board[7]==board[5]==board[3] and board[7]==letter:
+        return True
+    else:
+        return False
 def check_draw():
     if board.count(' ')<2: #nammude list il 10 space und  9 ennam use cheyyunullu adhyathe index ullath null aahnu so athu koodi include cheyyanam
         return True
     else:
         return False
+
+
 
 
 #oru position ilottu  letter insert cheyyumbol for eg 0 insert cheyyumbol
@@ -58,11 +78,13 @@ def insert(letter,pos):
                 exit()
         if check_draw():
             print("draw")
+            exit()
+
     else:
-        if letter=='0':
-            pos=int(input("Not free please enter a new position"))
-        else:
-            pos=random.randint(1,9)
+
+        pos=int(input("Not free please enter a new position"))
+
+
         insert(letter,pos)
 
 
@@ -76,11 +98,51 @@ def human_move(letter):
     pos=int(input("enter a number"))
     insert(letter,pos)
 def computer_move(letter):
-    pos=random.randint(1,9)
-    insert(letter,pos)
+    best_score=-100
+    best_pos=0
+    for index in range(1,len(board)):
+        if is_available(index):
+            board[index]=letter
+            score=minimax(board,False)
+            board[index]=" "
+            if score>best_score:
+                best_score=score
+                best_pos=index
+    insert(letter,best_pos)
+    return
+def minimax(board,is_maximizing):
+    if is_win(computer):
+        return 10
+    elif is_win(human):
+        return -10
+    elif check_draw():
+        return 0
+    if is_maximizing:
+        best_score = 100
 
+        for index in range(1, len(board)):
+            if is_available(index):
+                board[index] = computer
+                score = minimax(board, False)
+                board[index] = " "
+                best_score=max(best_score,score)
+        return best_score
+
+    else:
+        best_score = 100
+
+        for index in range(1, len(board)):
+            if is_available(index):
+                board[index] = human
+                score = minimax(board, True)
+                board[index] = " "
+                best_score=min(best_score,score)
+        return best_score
+
+
+
+#main loop
 while not check_win():
     display_board(board)
     computer_move(computer)
     human_move(human)
-
