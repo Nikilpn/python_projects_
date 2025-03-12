662b6430fbc131daa5404074a8179e06dd89b284
Nikhilpn <nikhilpnnick1234@gmail.com>
2025-03-12 08:18:16 -1200

    Higher_lowergame

---
+++ Higher_lower_game/game_database.py
@@ -0,0 +1,50 @@
+data=[
+    {
+        'name':'jenny',
+        'follower_count':1,
+        'description':'youtuber',
+        'country':'india'
+    },
+    {
+        'name': 'Cristiano',
+        'follower_count': 600,
+        'description': 'footballer',
+        'country': 'portugal'
+    },
+    {
+        'name': 'Narendra Modi',
+        'follower_count': 90,
+        'description': 'prime minister',
+        'country': 'india'
+    },
+    {
+        'name': "Fit Tuber",
+        'follower_count': 7,
+        'description': 'youtuber',
+        'country': 'india'
+    },
+    {
+        'name': "Mr.Beast",
+        'follower_count': 500,
+        'description': 'youtuber',
+        'country': 'America'
+    },
+    {
+        'name': "Priyanka Chopra",
+        'follower_count': 100,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': "Sharukhan",
+        'follower_count': 300,
+        'description': 'youtuber',
+        'country': 'India'
+    },
+    {
+        'name': "Hrithik Roshan",
+        'follower_count': 200,
+        'description': 'youtuber',
+        'country': 'America'
+    },
+]
